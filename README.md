e7e00155befbd0470ac7eba6e058cd85c840b223
MarcoAGreco <marcoandreagreco@gmail.com>
2022-09-19 16:47:40 +0200

    [CHK-617] feat: add soak test pipeline

---
+++ README.md
@@ -0,0 +1,29 @@
+# Tests for pagopa-ecommerce project
+This is a set of [k6](https://k6.io) load tests related to the pagopa-ecommerce services.
+
+All tests require a set of parameters: **rate**, **duration**, **preAllocatedVUs** and **maxVUs**. These parameters are necessary to set the test target to a given number of iteration per second (**rate**) in a given time (**duration**), using a certain number of VUs (**preAllocatedVUs** and **maxVUs**).
+
+To invoke k6 load test passing parameter use -e (or --env) flag:
+
+```
+-e MY_VARIABLE=MY_VALUE
+```
+
+Let's keep in mind that before perform a load test you have to:
+
+- run `yarn install`
+- run `yarn webpack`
+
+## 01. Soak tests
+
+This test require the dispatch of an email.
+
+```
+$ docker run -i --rm -v $(pwd)/dist:/dist  -e API_SUBSCRIPTION_KEY=${API_SUBSCRIPTION_KEY} -e URL_BASE_PATH=${URL_BASE_PATH} -e TEST_MAIL_FROM=${TEST_MAIL_FROM} -e TEST_MAIL_TO=${TEST_MAIL_TO} -e rate=${rate} -e duration=${duration} -e preAllocatedVUs=${preAllocatedVUs} -e maxVUs=${maxVUs} loadimpact/k6 run /dist/soak-test.js
+```
+
+To run test and load env vars fron `.env` file:
+
+```
+$ docker run -i --rm -v $(pwd)/dist:/dist  --env-file .env loadimpact/k6 run /dist/soak-test.js
+```
---
+++ src/soak-test/soak-test.ts
@@ -0,0 +1,58 @@
+import { check, fail, sleep } from "k6";
+import http from "k6/http";
+import { getConfigOrThrow } from "../utils/config";
+
+const config = getConfigOrThrow();
+
+export let options = {
+  scenarios: {
+    contacts: {
+      executor: "constant-arrival-rate",
+      rate: config.rate, // e.g. 20000 for 20K iterations
+      duration: config.duration, // e.g. '1m'
+      preAllocatedVUs: config.preAllocatedVUs, // e.g. 500
+      maxVUs: config.maxVUs, // e.g. 1000
+    },
+  },
+  thresholds: {
+    http_req_duration: ["p(99)<1500"], // 99% of requests must complete below 1.5s
+    "http_req_duration{api:checkMessages}": ["p(95)<1000"],
+  },
+};
+
+const params = {
+  headers: {
+    "Content-Type": "application/json",
+  },
+};
+
+export default function() {
+  const urlBasePath = config.URL_BASE_PATH;
+  const bodyRequest =  {
+    from: config.TEST_MAIL_FROM,
+    to: config.TEST_MAIL_TO,
+    templateId: "poc-1",
+    pspName: "pspName",
+    amount: 100,
+    transactionId: true
+  }
+
+  const headersParams = {
+    headers: {
+        'Content-Type': 'application/json',
+        'Ocp-Apim-Subscription-Key': config.API_SUBSCRIPTION_KEY
+    },
+};
+
+  let url = `${urlBasePath}/v1/emails`;
+  let res = http.post(url, JSON.stringify(bodyRequest), {
+    ...params,
+    tags: { api: "notifications-test" },
+  });
+  check(
+    res,
+    { "Response status from POST /emails was 200": (r) => r.status == 200 },
+    { tags: { api: "notifications-test" } }
+  );
+  
+}
