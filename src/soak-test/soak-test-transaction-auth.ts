83b42e6b8706633c55dc26187a216a4a18e09da7
Fernando-Granato <117268775+Fernando-Granato@users.noreply.github.com>
2023-02-22 17:41:45 +0100

    CHK-1131-auth-request-test (#18)
    * feet: added performance test for activation
    
    * feat: remuved ecommerce base path and added variable into pipeline
    
    * feat: fix problem pipeline
    
    * fix: test
    
    * added class for common methods
    
    * added export
    
    * Feat: refactoring auth request and verify method
    
    * Feat: added ramping-vus for verificaton end notifications test

--- src/soak-test/soak-test-transaction-auth.ts
+++ src/soak-test/soak-test-transaction-auth.ts
@@ -1,165 +1,93 @@
-import { check, fail} from "k6";
+import { check, fail } from "k6";
 import http from "k6/http";
 import { getConfigOrThrow } from "../utils/config";
+import { generateRptId, createActivationRequest, createAuthorizationRequest } from "../common/soak-test-common"
+import { NewTransactionResponse } from "../generated/ecommerce/NewTransactionResponse";
+import { TransactionStatusEnum } from "../generated/ecommerce/TransactionStatus";
 
 const config = getConfigOrThrow();
-
 export let options = {
     scenarios: {
         contacts: {
-            executor: "constant-arrival-rate",
-            rate: config.rate, // e.g. 20000 for 20K iterations
-            duration: config.duration, // e.g. '1m'
-            preAllocatedVUs: config.preAllocatedVUs, // e.g. 500
-            maxVUs: config.maxVUs, // e.g. 1000
+            executor: 'ramping-vus',
+            stages: [
+                { target: config.maxVUs, duration: config.rampingDuration },
+                { target: config.maxVUs, duration: config.duration },
+                { target: 0, duration: config.rampingDuration },
+            ],
         },
     },
+
     thresholds: {
         http_req_duration: ["p(99)<1500"], // 99% of requests must complete below 1.5s
-        "http_req_duration{api:GetPaymentRequestInfo}": ["p(95)<1000"],
-        "http_req_duration{api:CreateNewTransaction}": ["p(95)<1000"],
-        "http_req_duration{api:GetTransaction}": ["p(95)<1000"],
-        "http_req_duration{api:GetPaymentMethods}": ["p(95)<1000"],
-        "http_req_duration{api:GetPSPByPaymentMethod}": ["p(95)<1000"],
-        "http_req_duration{api:AuthRequest}": ["p(95)<1000"],
+        checks: ['rate>0.9'], // 90% of the request must be completed
+        "http_req_duration{api:activate-transaction-test}": ["p(95)<1500"],
+        "http_req_duration{api:get-transaction-test}": ["p(95)<1000"],
+        "http_req_duration{api:authorization-transaction-test}": ["p(95)<1500"]
     },
 };
 
-function generateRptId() {
-    var result = '77777777777302001';
-    for (var i = 0; i < 12; i++) {
-        result = result.concat((Math.floor(Math.random() * 10)).toString());
-    }
-    return result;
-}
-
 export default function () {
-    const urlBasePath = config.URL_BASE_PATH
-    let response
-    var rptId = generateRptId();
-    var paymentContextCode;
     var transactionId;
-    var amount = 1000;
-    var paymentMethodId;
-    var paymentToken;
-    var pspId;
-    var emailAddress = config.TEST_MAIL_TO
-    // Get Payment Request Info NM3
-    response = http.get(
-        `${urlBasePath}/checkout/ecommerce/v1/payment-requests/${rptId}?recaptchaResponse=test`, 
-         {tags: { api: "GetPaymentRequestInfo" }}
-    )
-    check(response, 
-        {'Response status from GET /payment-requests is 200': (r) => r.status == 200},
-        { api: "GetPaymentRequestInfo" }
+    const urlBasePath = config.URL_BASE_PATH;
+    const rptId = generateRptId();
+    const bodyRequest = createActivationRequest(rptId);
+
+    const headersParams = {
+        headers: {
+            'Content-Type': 'application/json',
+            'Ocp-Apim-Subscription-Key': config.API_SUBSCRIPTION_KEY,
+            'Authorization': "Bearer "
+        },
+    };
+
+    let url = `${urlBasePath}/transactions`;
+    let response = http.post(url, JSON.stringify(bodyRequest), {
+        ...headersParams,
+        tags: { api: "activate-transaction-test" },
+    });
+
+    check(
+        response,
+        { "Response status from POST /transactions was 200": (r) => r.status == 200 },
+        { api: "activate-transaction-test" }
+    );
+
+    if (response.status == 200 && response.json() != null) {
+        var body = response.json() as unknown as NewTransactionResponse;
+        transactionId = body.transactionId;
+        headersParams.headers.Authorization = headersParams.headers.Authorization + body.authToken as string;
+
+        url = `${urlBasePath}/transactions/${transactionId}`;
+        response = http.get(url, {
+            ...headersParams,
+            tags: { api: "get-transaction-test" }
+        });
+
+        check(
+            response,
+            { "Response status from GET /transactions by transaction id was 200": (r) => r.status == 200 },
+            { api: "get-transaction-test" }
         );
-    if (response.status == 200) {
-        paymentToken = response.json()["paymentToken"];
-        rptId = response.json()["rptId"];
-        paymentContextCode = response.json()["paymentContextCode"];
-    }
-    if (paymentContextCode !== undefined && amount !== undefined) {
-        // Create new transaction
-        const bodyRequest = {
-            "rptId": rptId,
-            "paymentContextCode": paymentContextCode,
-            "email": emailAddress,
-            "amount": amount
+
+        if (body.status === TransactionStatusEnum.ACTIVATED) {
+            url = `${urlBasePath}/transactions/${transactionId}/auth-requests`;
+            // Authorization request
+            const bodyRequest = createAuthorizationRequest();
+            let response = http.post(url, JSON.stringify(bodyRequest), {
+                ...headersParams,
+                tags: { api: "authorization-transaction-test" },
+            });
+
+            check(response,
+                { 'Response status from POST /transactions/{transactionId}/auth-requests is 200': (r) => r.status == 200 },
+                { api: "AuthRequest" }
+            );
+        } else {
+            fail('Error into authorization request');
         }
 
-        response = http.post(
-            `${urlBasePath}/checkout/ecommerce/v1/transactions?test=debug`,
-            JSON.stringify(bodyRequest),
-            {
-                headers: {
-                    'content-type': 'application/json',
-                },
-                tags: { api: "CreateNewTransaction" }
-            }
-        )
-        check(response, 
-            {'rResponse status from POST /transactions is 200': (r) => r.status == 200},
-            { api: "CreateNewTransaction" });
     } else {
-        fail('Missing data for create new transaction');
-    }
-    if (response.status == 200) {
-        transactionId = response.json()["transactionId"];
-    }
-
-    if (transactionId !== undefined) {
-        // Get transaction
-        response = http.get(
-            `${urlBasePath}/checkout/ecommerce/v1/transactions/${transactionId}`,
-            {tags: { api: "GetTransaction" }}
-        )
-        check(response, 
-            {'Response status from GET /transactions is 200': (r) => r.status == 200},
-            { api: "GetTransaction" }
-            );
-    } else {
-        fail('Missing data for get transaction');
-    }
-
-    if (response.status == 200) {
-        paymentToken = response.json()["paymentToken"];
-    }
-    // Get payment methods
-    if (amount !== undefined) {
-        response = http.get(
-            `${urlBasePath}/checkout/ecommerce/v1/payment-methods?amount=${amount}`,
-            {tags: { api: "GetPaymentMethods" }}
-        )
-        check(response, 
-            {'Response status from GET /payment-methods is 200': (r) => r.status == 200},
-            {api: "GetPaymentMethods" }
-            );
-    } else {
-        fail('Missing data for get payment methods');
-    }
-    if (response.status == 200) {
-        paymentMethodId = response.json()[0].id
-    }
-    // Get PSPs by payment method
-    if (paymentMethodId !== undefined && amount !== undefined) {
-        response = http.get(
-            `${urlBasePath}/checkout/ecommerce/v1/payment-methods/${paymentMethodId}/psps?amount=${amount}`,
-            {tags: { api: "GetPSPByPaymentMethod" }}
-        )
-        check(response, 
-            {'Response status from GET /payment-methods is 200': (r) => r.status == 200},
-            {api: "GetPSPByPaymentMethod" }
-            );
-        if (response.status == 200) {
-            pspId = response.json()["psp"][0]["code"];
-        }
-    } else {
-        fail('Missing data for get PSPs by payment method')
-    }
-    if (transactionId !== undefined && amount !== undefined && pspId !==undefined) {
-        // Authorization request
-        const bodyRequest = {
-            "amount": amount,
-            "fee": 1.0,
-            "paymentInstrumentId": paymentMethodId,
-            "pspId": pspId,
-            "language": "IT"
-        }
-        response = http.post(
-            `${urlBasePath}/checkout/ecommerce/v1/transactions/${transactionId}/auth-requests`,
-            JSON.stringify(bodyRequest),
-            {
-                headers: {
-                    'content-type': 'application/json',
-                },
-                tags: { api: "AuthRequest" }
-            }
-        )
-        check(response, 
-            {'Response status from POST /transactions/{transactionId}/auth-requests is 200': (r) => r.status == 200},
-            { api: "AuthRequest" }
-            );
-    } else {
-        fail('Missing data for authorization request');
+        fail('Error into activation request');
     }
 }
--- src/soak-test/soak-test-verification.ts
+++ src/soak-test/soak-test-verification.ts
@@ -1,16 +1,20 @@
-import {hitCacheTest} from "../common/soak-test-verification-common"
 import { getConfigOrThrow } from "../utils/config";
+import http from "k6/http";
+import { check } from "k6";
+import { generateRptId } from "../common/soak-test-common"
+
 
 const config = getConfigOrThrow();
 
 export let options = {
   scenarios: {
-    hitCache: {
-      executor: "constant-arrival-rate",
-      rate: config.rate, // e.g. 20000 for 20K iterations
-      duration: config.duration, // e.g. '1m'
-      preAllocatedVUs: config.preAllocatedVUs, // e.g. 500
-      maxVUs: config.maxVUs, // e.g. 1000
+    contacts: {
+      executor: 'ramping-vus',
+      stages: [
+        { target: config.maxVUs, duration: config.rampingDuration },
+        { target: config.maxVUs, duration: config.duration },
+        { target: 0, duration: config.rampingDuration },
+      ],
     },
   },
   thresholds: {
@@ -19,7 +23,35 @@ export let options = {
   },
 };
 
-
-export default function(){
-  hitCacheTest(false);
+export function hitCacheTest() {
+  const urlBasePath = config.URL_BASE_PATH;
+  const rptId = generateRptId();
+  let url = `${urlBasePath}/payment-requests/${rptId}?recaptchaResponse=test`;
+  // Get Payment Request Info NM3
+  let response = http.get(
+    url,
+    {
+      tags: { api: 'PaymentRequest-verify' },
+    }
+  );
+  check(
+    response,
+    { "Response status from GET /payment-request was 200": (r) => r.status == 200 },
+    { api: 'PaymentRequest-verify' }
+  );
+  //make a verify request for the same rptId to test cache hit response time
+  response = http.get(
+    url,
+    {
+      tags: { api: `PaymentRequest-verify-hitCache` },
+    }
+  );
+  check(
+    response,
+    { "Response status from GET /payment-request was 200": (r) => r.status == 200 },
+    { api: `PaymentRequest-verify-hitCache` }
+  );
 }
+
+
+
