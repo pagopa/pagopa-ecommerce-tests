9eab25a4d30fe2f15af4289ba111149fb3502b0b
Fernando-Granato <fernando.granato@pagopa.it>
2023-02-22 14:23:57 +0100

    Feat: added ramping-vus for verificaton end notifications test

--- src/soak-test/soak-test-verification.ts
+++ src/soak-test/soak-test-verification.ts
@@ -8,17 +8,18 @@ const config = getConfigOrThrow();
 
 export let options = {
   scenarios: {
-    hitCacheNm3: {
-      executor: "constant-arrival-rate",
-      rate: config.rate, // e.g. 20000 for 20K iterations
-      duration: config.duration, // e.g. '1m'
-      preAllocatedVUs: config.preAllocatedVUs, // e.g. 500
-      maxVUs: config.maxVUs, // e.g. 1000
-    }
+    contacts: {
+      executor: 'ramping-vus',
+      stages: [
+        { target: config.maxVUs, duration: config.rampingDuration },
+        { target: config.maxVUs, duration: config.duration },
+        { target: 0, duration: config.rampingDuration },
+      ],
+    },
   },
   thresholds: {
-    "http_req_duration{api:PaymentRequest-verify-NM3}": ["p(95)<1000"],//95% of requests must complete below 1.0s
-    "http_req_duration{api:PaymentRequest-verify-NM3-hitCache}": ["p(95)<1000"],//95% of requests must complete below 1.0s
+    "http_req_duration{api:PaymentRequest-verify}": ["p(95)<1000"],//95% of requests must complete below 1.0s
+    "http_req_duration{api:PaymentRequest-verify-hitCache}": ["p(95)<1000"],//95% of requests must complete below 1.0s
   },
 };
 
