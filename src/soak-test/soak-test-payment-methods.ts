389f6dc52d9bd5a6b9d98f21ea584222ca86bf02
Simone Infante <simone.infante@pagopa.it>
2023-03-28 20:20:37 +0200

    fix: paths

--- src/soak-test/soak-test-payment-methods.ts
+++ src/soak-test/soak-test-payment-methods.ts
@@ -17,7 +17,9 @@ export let options = {
     thresholds: {
         http_req_duration: ["p(99)<1500"], // 99% of requests must complete below 1.5s
         checks: ['rate>0.9'], // 90% of the request must be completed
-        "http_req_duration{api:get-payment-methods}": ["p(95)<1000"]
+        "http_req_duration{api:get-payment-methods}": ["p(95)<1000"],
+        "http_req_duration{api:get-payment-method-by-id}": ["p(95)<1000"],
+        "http_req_duration{api:post-fees}": ["p(95)<1000"]
     },
 };
 
@@ -26,14 +28,57 @@ let counter: number = 0;
 export default function () {
     const urlBasePath = config.URL_BASE_PATH;
 
+    const filterAmount = "10000"
+
     // Test for GET all payment methods
-    let url = `${urlBasePath}/ecommerce/checkout/v1/payment-methods`;
-    let response = http.post(url, { tags: { api: "get-payment-methods" },
+    const paymentMethodsResponse = http.post(
+        `${urlBasePath}/ecommerce/checkout/v1/payment-methods?amount=${filterAmount}`, 
+        { tags: { api: "get-payment-methods" },
     });
 
     check(
-        response,
-        { "Response status from POST /payment-methods was 200": (r) => r.status == 200 },
+        paymentMethodsResponse,
+        { "Response status from GET /payment-methods was 200": (r) => r.status == 200 },
         { api: "get-payment-methods" }
+    );
+
+    const firstPaymentMethodId = paymentMethodsResponse.json()["id"]
+
+    // Test for GET  payment method by ud
+    const paymentMethodResponse = http.post(
+        `${urlBasePath}/ecommerce/checkout/v1/payment-methods/${firstPaymentMethodId}`, 
+        { tags: { api: "get-payment-method-by-id" },
+    });
+
+    check(
+        paymentMethodResponse,
+        { "Response status from GET /payment-methods/:id was 200": (r) => r.status == 200 },
+        { api: "get-payment-method-by-id" }
+    );
+
+    // Test for POST fees  payment method by ud
+    const feesResponse = http.post(
+        `${urlBasePath}/ecommerce/checkout/v1/payment-methods/${firstPaymentMethodId}/fees`, 
+        { tags: { api: "post-fees" },
+    });
+
+    // Body request for POST fees
+    const bodyCreatePaymentMethodRequest = {
+        bin: "400000",
+        touchpoint: "CHECKOUT",
+        paymentAmount: "10000",
+        primaryCreditorInstitution: "66666666666",
+        transferList : [
+            { 
+              creditorInstitution : "66666666666",
+              digitalStamp : false
+            }
+        ]
+    }
+
+    check(
+        paymentMethodResponse,
+        { "Response status from GET /payment-methods/:id/fees was 200": (r) => r.status == 200 },
+        { api: "post-fees" }
     );
 }
