b7918daf3bf28f130715ea9e5e7d55eb7979b275
Pietro Tota <pietro.tota@pagopa.it>
2023-06-28 10:57:55 +0200

    fix: add soak tests for payment methods

--- src/common/soak-test-common.ts
+++ src/common/soak-test-common.ts
@@ -1,6 +1,8 @@
 import { AmountEuroCents } from "../generated/ecommerce/AmountEuroCents";
+import { CalculateFeeRequest } from "../generated/ecommerce/CalculateFeeRequest";
 import { NewTransactionRequest } from "../generated/ecommerce/NewTransactionRequest";
 import { PaymentContextCode } from "../generated/ecommerce/PaymentContextCode";
+import { BrandEnum } from "../generated/ecommerce/PaymentInstrumentDetail";
 import { PaymentNoticeInfo } from "../generated/ecommerce/PaymentNoticeInfo";
 import { LanguageEnum, RequestAuthorizationRequest } from "../generated/ecommerce/RequestAuthorizationRequest";
 import { RptId } from "../generated/ecommerce/RptId";
@@ -22,12 +24,12 @@ export const createActivationRequest = (
     paymentNotices: Array(5).fill("").map(paymentNotice)
 });
 
-export const paymentNotice =()
-:PaymentNoticeInfo => ({
-    rptId: generateRptId() as RptId,
-    amount: 1000 as AmountEuroCents,
-    paymentContextCode: "6cd9114e-6427-4932-9a27-96168640d944" as PaymentContextCode
-})
+export const paymentNotice = ()
+    : PaymentNoticeInfo => ({
+        rptId: generateRptId() as RptId,
+        amount: 1000 as AmountEuroCents,
+        paymentContextCode: "6cd9114e-6427-4932-9a27-96168640d944" as PaymentContextCode
+    })
 
 export const createAuthorizationRequest = (
 ): RequestAuthorizationRequest => ({
@@ -36,8 +38,9 @@ export const createAuthorizationRequest = (
     pspId: "PSP_ILA",
     language: LanguageEnum.IT,
     paymentInstrumentId: "79e01170-5af9-4d4a-9a3b-c9cbca3af60e",
+    isAllCCP: true,
     details: {
-        brand: "visa",
+        brand: BrandEnum.VISA,
         cvv: "123",
         detailType: "card",
         expiryDate: "203012",
@@ -45,5 +48,17 @@ export const createAuthorizationRequest = (
         pan: "4000000000000101",
         threeDsData: "{\"browserJavaEnabled\":\"false\",\"browserLanguage\":\"it-IT\",\"browserColorDepth\":\"24\",\"browserScreenHeight\":\"1440\",\"browserScreenWidth\":\"3440\",\"browserTZ\":\"-60\",\"browserAcceptHeader\":\"*/*\",\"browserIP\":\"93.67.147.246\",\"browserUserAgent\":\"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36\",\"acctID\":\"ACCT_9e3d49d3-f337-4bdc-97e2-031cd5ae01d8\",\"deliveryEmailAddress\":\"mario.rossi@email.com\",\"mobilePhone\":null}"
     }
+});
 
+export const calculateFeeRequest = (
+): CalculateFeeRequest => ({
+    touchpoint: "CHECKOUT",
+    bin: "511111",
+    paymentAmount: 100,
+    primaryCreditorInstitution: "77777777777",
+    transferList: [{
+        creditorInstitution: "77777777777",
+        digitalStamp: false,
+    }],
+    isAllCCP: true,
 });
--- src/soak-test/soak-test-payment-methods.ts
+++ src/soak-test/soak-test-payment-methods.ts
@@ -1,30 +1,41 @@
 import { check, fail } from "k6";
 import http from "k6/http";
 import { getConfigOrThrow } from "../utils/config";
+import { PaymentMethodResponse } from "../generated/ecommerce/PaymentMethodResponse";
+import { PaymentMethodsResponse } from "../generated/ecommerce/PaymentMethodsResponse";
+import { calculateFeeRequest } from "../common/soak-test-common";
 
 const config = getConfigOrThrow();
 
 export let options = {
     scenarios: {
-      contacts: {
-        executor: 'ramping-arrival-rate',
-        startRate: 0,
-        timeUnit: '1s',
-        preAllocatedVUs: config.preAllocatedVUs,
-        maxVUs: config.maxVUs,
-        stages: [
-          { target: config.rate, duration: config.rampingDuration },
-          { target: config.rate, duration: config.duration },
-          { target: 0, duration: config.rampingDuration },
-        ],
-      },
+        contacts: {
+            executor: 'ramping-arrival-rate',
+            startRate: 0,
+            timeUnit: '1s',
+            preAllocatedVUs: config.preAllocatedVUs,
+            maxVUs: config.maxVUs,
+            stages: [
+                { target: config.rate, duration: config.rampingDuration },
+                { target: config.rate, duration: config.duration },
+                { target: 0, duration: config.rampingDuration },
+            ],
+        },
     },
     thresholds: {
         http_req_duration: ["p(99)<1500"], // 99% of requests must complete below 1.5s
         checks: ['rate>0.9'], // 90% of the request must be completed
         "http_req_duration{name:get-payment-methods}": ["p(95)<1000"],
         "http_req_duration{name:get-payment-method-by-id}": ["p(95)<1000"],
-        "http_req_duration{name:post-fees}": ["p(95)<1000"]
+        "http_req_duration{name:calculate-fees}": ["p(95)<1000"]
+    },
+};
+
+const headersParams = {
+    headers: {
+        'Content-Type': 'application/json',
+        'Ocp-Apim-Subscription-Key': config.API_SUBSCRIPTION_KEY,
+        'Authorization': "Bearer "
     },
 };
 
@@ -34,63 +45,56 @@ export default function () {
     const filterAmount = "10000"
 
     // Test for GET all payment methods
-    const paymentMethodsResponse = http.get(
-        `${urlBasePath}/payment-methods?amount=${filterAmount}`, 
-        { tags: { name: "get-payment-methods" },
-    });
+    let response = http.get(
+        `${urlBasePath}/payment-methods?amount=${filterAmount}`,
+        {
+            ...headersParams,
+            tags: { name: "get-payment-methods" },
+        });
 
     check(
-        paymentMethodsResponse,
+        response,
         { "Response status from GET /payment-methods was 200": (r) => r.status == 200 },
         { name: "get-payment-methods" }
     );
-/*
-    if ( paymentMethodsResponse.status == 200 && paymentMethodsResponse.json() !== undefined ) {
 
-
-        (<type.JSONArray>paymentMethodsResponse.json()).forEach(function (firstPaymentMethod: type.JSONObject) {
-           
-            let paymentMethodId = (<type.JSONObject>firstPaymentMethod)["id"];
-
-            // Test for GET  payment method by ud
-            const paymentMethodResponse = http.get(
-                `${urlBasePath}/payment-methods/${paymentMethodId}`, 
-                { tags: { name: "get-payment-method-by-id" },
-            });
-
+    if (response.status == 200 && response.json() !== undefined) {
+        const paymentMethods = response.json() as unknown as PaymentMethodsResponse;
+        if (paymentMethods.paymentMethods) {
+            const firstPaymentMethod = paymentMethods.paymentMethods[0] as unknown as PaymentMethodResponse;
+            // Test for GET payment method by id
+            response = http.get(
+                `${urlBasePath}/payment-methods/${firstPaymentMethod.id}`,
+                {
+                    ...headersParams,
+                    tags: { name: "get-payment-method-by-id" },
+                });
             check(
-                paymentMethodResponse,
-                { "Response status from GET /payment-methods/:id was 200": (r) => r.status == 200 },
+                response,
+                { "Response status from GET /payment-methods/{id} was 200": (r) => r.status == 200 },
                 { name: "get-payment-method-by-id" }
             );
-
-            // Test for POST fees  payment method by ud
-            const feesResponse = http.post(
-                `${urlBasePath}/payment-methods/${firstPaymentMethodId}/fees`, 
-                { tags: { name: "post-fees" },
-            });
-
-            // Body request for POST fees
-            const bodyCreatePaymentMethodRequest = {
-                bin: "400000",
-                touchpoint: "CHECKOUT",
-                paymentAmount: "10000",
-                primaryCreditorInstitution: "66666666666",
-                transferList : [
-                    { 
-                        creditorInstitution : "66666666666",
-                        digitalStamp : false
-                    }
-                ]
-            }
-
+            const calculateFee = calculateFeeRequest()
+            
+            //Test calculate fees
+            response = http.post(
+                `${urlBasePath}/payment-methods/${firstPaymentMethod.id}/fees`,
+                JSON.stringify(calculateFee),
+                {
+                    ...headersParams,
+                    tags: { name: "calculate-fees" },
+                });
             check(
-                paymentMethodResponse,
-                { "Response status from GET /payment-methods/:id/fees was 200": (r) => r.status == 200 },
-                { name: "post-fees" }
+                response,
+                { "Response status from POST /payment-methods/{id}/fees was 200": (r) => r.status == 200 },
+                { name: "calculate-fees" }
             );
-        });
 
+        } else {
+            fail("Get all payment methods returned no payment method!");
+        }
 
-    }*/
+    } else {
+        fail(`Cannot retrieve all payment methods, received error code: ${response.status} and body ${response.body}`)
+    }
 }
